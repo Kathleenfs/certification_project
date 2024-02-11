5bed1969ac00f2b476bdcef73cea96b83422a382
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2024-02-10 21:36:48 -0300

    student entity created

---
+++ src/main/java/br/com/kathleenfss/certification_project/modules/students/entities/StudentEntity.java
@@ -0,0 +1,5 @@
+package br.com.kathleenfss.certification_project.modules.students.entities;
+
+public class StudentEntity {
+
+}
---
+++ src/test/java/br/com/kathleenfss/certification_project/CertificationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kathleenfss.certification_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
