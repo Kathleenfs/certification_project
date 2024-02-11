584823bf1ee698b2d7dc33ce111a2f98919794da
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2024-02-11 17:54:24 -0300

    created database postgres

--- src/main/java/br/com/kathleenfss/certification_project/modules/students/entities/StudentEntity.java
+++ src/main/java/br/com/kathleenfss/certification_project/modules/students/entities/StudentEntity.java
@@ -3,6 +3,7 @@ package br.com.kathleenfss.certification_project.modules.students.entities;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class StudentEntity {
 
     private UUID id;
