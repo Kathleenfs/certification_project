95de54b2674faf8880efb7e698383be1143121d7
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2024-02-11 10:27:06 -0300

    student entity attributes

--- src/main/java/br/com/kathleenfss/certification_project/modules/students/entities/StudentEntity.java
+++ src/main/java/br/com/kathleenfss/certification_project/modules/students/entities/StudentEntity.java
@@ -1,5 +1,12 @@
 package br.com.kathleenfss.certification_project.modules.students.entities;
 
+import java.util.List;
+import java.util.UUID;
+
 public class StudentEntity {
+
+    private UUID id;
+    private String email;
+    private List<CertificationStudentEntity> certificationStudentEntity;
 
 }
