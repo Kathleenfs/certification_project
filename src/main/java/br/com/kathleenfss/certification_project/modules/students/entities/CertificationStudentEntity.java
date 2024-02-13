9b41a9bddede9e960d0f0464865a13964de45c26
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2024-02-12 21:43:41 -0300

    relationship OneToMany/ManyToOne

--- src/main/java/br/com/kathleenfss/certification_project/modules/students/entities/CertificationStudentEntity.java
+++ src/main/java/br/com/kathleenfss/certification_project/modules/students/entities/CertificationStudentEntity.java
@@ -3,6 +3,13 @@ package br.com.kathleenfss.certification_project.modules.students.entities;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,12 +17,26 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "certifications")
 public class CertificationStudentEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    private UUID studentID;
+
+    @Column(length = 100)
     private String technology;
+
+    @Column(length = 100)
     private int grade;
-    List<AnswersCertificationsEntity> answersCertificationsEntity;
+
+    @JoinColumn(name = "student_id")
+    private UUID studentID;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", insertable = false, updatable = false)
+    private StudentEntity studentEntity;
+
+    // List<AnswersCertificationsEntity> answersCertificationsEntity;
 
 }
--- src/main/java/br/com/kathleenfss/certification_project/modules/students/entities/StudentEntity.java
+++ src/main/java/br/com/kathleenfss/certification_project/modules/students/entities/StudentEntity.java
@@ -3,7 +3,12 @@ package br.com.kathleenfss.certification_project.modules.students.entities;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,11 +16,17 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
+@Entity(name = "students")
 public class StudentEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @OneToMany(mappedBy = "studentEntity")
     private List<CertificationStudentEntity> certificationStudentEntity;
 
 }
