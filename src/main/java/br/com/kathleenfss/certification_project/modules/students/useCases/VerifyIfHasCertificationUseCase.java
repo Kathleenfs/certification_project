21e022e7fcc1eabe2d009a779d50d488c6dd79f3
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2024-02-13 20:30:52 -0300

    created repositories

--- src/main/java/br/com/kathleenfss/certification_project/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/br/com/kathleenfss/certification_project/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,14 +1,21 @@
 package br.com.kathleenfss.certification_project.modules.students.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.kathleenfss.certification_project.modules.students.dto.VerifyHasCertificationDTO;
+import br.com.kathleenfss.certification_project.modules.students.repositories.CertificationStudentRepository;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
 
+    @Autowired
+    private CertificationStudentRepository certificationStudentRepository;
+
     public boolean execute(VerifyHasCertificationDTO dto) {
-        if (dto.getEmail().equals("kathleenfss@gmail.com") && dto.getTechnology().equals("JAVA")) {
+        var result = this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(),
+                dto.getTechnology());
+        if (!result.isEmpty()) {
             return true;
         }
         return false;
