f23a17996fc9ab06d4409ba568a7254aee115052
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2024-02-13 19:52:33 -0300

    relationship OneToMany/ManyToOne

--- src/main/java/br/com/kathleenfss/certification_project/modules/students/entities/CertificationStudentEntity.java
+++ src/main/java/br/com/kathleenfss/certification_project/modules/students/entities/CertificationStudentEntity.java
@@ -1,7 +1,10 @@
 package br.com.kathleenfss.certification_project.modules.students.entities;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -10,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -37,6 +41,11 @@ public class CertificationStudentEntity {
     @JoinColumn(name = "student_id", insertable = false, updatable = false)
     private StudentEntity studentEntity;
 
-    // List<AnswersCertificationsEntity> answersCertificationsEntity;
+    @OneToMany
+    @JoinColumn(name = "answer_certification_id", insertable = false, updatable = false)
+    List<AnswersCertificationsEntity> answersCertificationsEntity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
